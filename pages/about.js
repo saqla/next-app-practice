c8381aa3a5cfea652892a43b4cdfc74f545f61d1
saqla <whitecross8fortune8fatedress@gmail.com>
2022-07-28 23:00:35 +0900

    header & footerの状態保持可能なレイアウト

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,7 @@
-import Layout from "../components/Layout";
-
 export default function About() {
     return (
-        <Layout>
+        <div>
             <h1>About Page</h1>
-        </Layout>
+        </div>
     );
 }
