f309d988a0a03a5a25e56acc9a8e02fc01b2834c
saqla <whitecross8fortune8fatedress@gmail.com>
2022-07-28 22:29:09 +0900

    Layoutを_app.jsに設定

--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,6 @@ const products = [{ name: "bag"}, { name: "shoes"}, { name: "socks"} ];
 export default function Home() {
   return (
     <div>
-      <ul>
-        {products.map((product) => {
-          return (
-            <li key={product.name}>
-              <Link href={`/products/${product.name}`} >
-                <a>{product.name}</a>
-              </Link>
-            </li>
-          )
-        })}
-        <li>
-          <Link href="/about">
-            <a>About</a>
-          </Link>
-        </li>
-      </ul>
       <h1>Hello Next.js</h1>
     </div>
   );
