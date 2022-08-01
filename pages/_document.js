babee0f5722ec0c4d1cf10f2f7acd14b6cd135e0
saqla <whitecross8fortune8fatedress@gmail.com>
2022-08-01 22:30:02 +0900

    フォント設定

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,12 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
     render() {
         return (
-            <Html lang="ja">
-                <Head />
+            <Html>
+                <Head>
+                    <link rel="preconnect" href="https://fonts.googleapis.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+                    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" />
+                </Head>
                 <body>
                     <Main />
                     <div id="portal"></div>
