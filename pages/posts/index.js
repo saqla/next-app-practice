d6fef800747e692fcb4e2ae6a37979877aec61f4
saqla <whitecross8fortune8fatedress@gmail.com>
2022-08-01 19:46:39 +0900

    SWRを用いた場合

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,26 +1,53 @@
-import Link from "next/link";
+// import Link from "next/link";
 
-export default function index({ posts }) {
+// export default function index({ posts }) {
+//     return (
+//         <div>
+//             <h1>POST一覧</h1>
+//             <ul>
+//                 {posts.map((post) => {
+//                     return (
+//                         <li key={post.id}>
+//                             <Link href={`/posts/${post.id}`}>
+//                                 <a>{post.title}</a>
+//                             </Link>
+//                         </li>
+//                     );
+//                 })};
+//             </ul>
+//         </div>
+//     );
+// }
+
+// export async function getStaticProps() {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//     const posts = await res.json();
+//     return { props: { posts } } 
+// }
+
+
+
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
+export default function index() {
+    const { data, error } = useSWR(
+        'https://jsonplaceholder.typicode.com/posts',
+        fetcher
+    );
+
+    if (error) return <div>failed to load</div>;
+    if (!data) return <div>loading...</div>;
+
     return (
         <div>
             <h1>POST一覧</h1>
             <ul>
-                {posts.map((post) => {
-                    return (
-                        <li key={post.id}>
-                            <Link href={`/posts/${post.id}`}>
-                                <a>{post.title}</a>
-                            </Link>
-                        </li>
-                    );
-                })};
+                {data.map((post) => {
+                    return <li key={post.id}>{post.title}</li>;
+                })}
             </ul>
         </div>
     );
-}
-
-export async function getStaticProps() {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    const posts = await res.json();
-    return { props: { posts } } 
 }
